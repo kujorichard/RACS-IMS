1d2a0b2b8a0bb2f40c8fb2fd5859eee4ce9ae774
Kyle Sanchez <kylesanchez3000@gmail.com>
2025-02-05 22:52:42 +0800

    Added an Initial Draft For Products list object model and datagrid

---
+++ RAC-IMS/Backend MongoDB/MongoDBService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace RAC_IMS.Backend_MongoDB
+{
+    public class MongoDBService
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDBService(string connectionString, string databaseName)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        // Function for obtaining the collection of products object
+        public IMongoCollection<Product> GetProductsCollection()
+        {
+            return _database.GetCollection<Product>("Products");
+        }
+    }
+}
---
+++ RAC-IMS/Backend MongoDB/ObjectModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAC_IMS.Backend_MongoDB
+{
+    // Model for the Product object in the database
+    public class Product
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public double reseller_price { get; set; }
+        public double wholesale_price { get; set; }
+        public double retail_price { get; set; }
+        public string category { get; set; }
+        public int supplier_id { get; set; }
+    }
+
+}
